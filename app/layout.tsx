a45fb19ac0e581a5c0c2a966e3273b7066f31091
Masanori Takeuchi <m.takeuchi@open-face.net>
2025-04-10 17:22:18 +0900

    Revert "2025/04/10 R1"
    This reverts commit 177e71c105fe8214ab4372922cc8dd29b15d4eb1.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,22 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import Script from "next/script";
-import Navbar from "@/components/Navbar";
-
-const inter = Inter({ subsets: ["latin"] });
+import type { Metadata } from 'next'
+import './globals.css'
 
 export const metadata: Metadata = {
-  title: "Your Company - ホームページ",
-  description: "革新的なソリューションで、あなたのビジネスの成長をサポートします。",
-};
+  title: 'OPEN FACE LLC',
+  description: 'ビジネス変革でデータ価値を最大化させるパートナー',
+  icons: {
+    icon: '/images/openface.ico',
+  },
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="ja" className="scroll-smooth">
-      <head>
-        <Script
-          src="https://challenges.cloudflare.com/turnstile/v0/api.js"
-          async
-          defer
-        />
-      </head>
-      <body className={`${inter.className} pt-16`}>
-        <Navbar />
-        {children}
-      </body>
+    <html lang="en">
+      <body>{children}</body>
     </html>
-  );
+  )
 }
